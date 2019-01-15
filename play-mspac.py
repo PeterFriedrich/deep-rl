93273da7dfbecb4e1942585fceea1e9da3e0a145
Peter <peter-friedrich@hotmail.com>
2019-01-15 15:21:49 -0700

    Added files via upload

---
+++ play-mspac.py
@@ -0,0 +1,195 @@
+"""
+Play a game using the trained Ms-pac agent
+
+"""
+
+# Common imports
+import numpy as np
+import os
+import sys
+import datetime
+
+# main libraries
+import gym 
+import tensorflow as tf 
+
+# plot things 
+import matplotlib
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+plt.rcParams['axes.labelsize'] = 14
+plt.rcParams['xtick.labelsize'] = 12
+plt.rcParams['ytick.labelsize'] = 12
+
+# Where to save the figures
+PROJECT_ROOT_DIR = "."
+
+def save_fig(fig_id, tight_layout=True):
+    path = os.path.join(PROJECT_ROOT_DIR, "images", fig_id + ".png")
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format='png', dpi=300)
+    
+# Animate stuff 
+def update_scene(num, frames, patch):
+    patch.set_data(frames[num])
+    return patch,
+    
+def plot_animation(frames, repeat=False, interval=40):
+    plt.close()  # or else nbagg sometimes plots in the previous cell
+    fig = plt.figure()
+    patch = plt.imshow(frames[0])
+    plt.axis('off')
+    return animation.FuncAnimation(fig, update_scene, fargs=(frames, patch), frames=len(frames), repeat=repeat, interval=interval)
+
+# Environment 
+env = gym.make("MsPacman-v0")
+obs = env.reset()
+
+# Preprocess the environment 
+mspacman_color = 210 + 164 + 74
+
+def preprocess_observation(obs):
+    img = obs[1:176:2, ::2] # crop and downsize
+    img = img.sum(axis=2) # to greyscale
+    img[img==mspacman_color] = 0 # Improve contrast
+    img = (img // 3 - 128).astype(np.int8) # normalize from -128 to 127
+    return img.reshape(88, 80, 1)
+
+img = preprocess_observation(obs)
+
+# Time information
+timelog = []
+timelog.append(datetime.datetime.now())
+
+""" 
+---------------------------------------------------------------------------------------------------------------------------------------
+Building the DQN System
+---------------------------------------------------------------------------------------------------------------------------------------
+"""
+
+
+# setting up inputs for convolutional network 
+input_height = 88
+input_width = 80
+input_channels = 1
+conv_n_maps = [32, 64, 64]
+conv_kernel_sizes = [(8,8), (4,4), (3,3)]
+conv_strides = [4, 2, 1]
+conv_paddings = ["SAME"] * 3 
+conv_activation = [tf.nn.relu] * 3
+n_hidden_in = 64 * 11 * 10  # conv3 has 64 maps of 11x10 each
+n_hidden = 512
+hidden_activation = tf.nn.relu
+n_outputs = env.action_space.n  # 9 discrete actions are available
+initializer = tf.variance_scaling_initializer()
+
+def q_network(X_state, name):
+    prev_layer = X_state / 128.0 # scale pixel intensities to the [-1.0, 1.0] range.
+    with tf.variable_scope(name) as scope:
+        for n_maps, kernel_size, strides, padding, activation in zip(
+                conv_n_maps, conv_kernel_sizes, conv_strides,
+                conv_paddings, conv_activation):
+            prev_layer = tf.layers.conv2d(
+                prev_layer, filters=n_maps, kernel_size=kernel_size,
+                strides=strides, padding=padding, activation=activation,
+                kernel_initializer=initializer)
+        last_conv_layer_flat = tf.reshape(prev_layer, shape=[-1, n_hidden_in])
+        hidden = tf.layers.dense(last_conv_layer_flat, n_hidden,
+                                 activation=hidden_activation,
+                                 kernel_initializer=initializer)
+        #Optional Extra layer                          
+        hidden_2 = tf.layers.dense(hidden, n_hidden,
+                                 activation=hidden_activation,
+                                 kernel_initializer=initializer)
+        outputs = tf.layers.dense(hidden_2, n_outputs, 
+                                  kernel_initializer=initializer)
+        
+        #outputs = tf.layers.dense(hidden, n_outputs,
+         #                         kernel_initializer=initializer)
+    trainable_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,
+                                       scope=scope.name)
+    trainable_vars_by_name = {var.name[len(scope.name):]: var
+                              for var in trainable_vars}
+    return outputs, trainable_vars_by_name
+    
+X_state = tf.placeholder(tf.float32, shape=[None, input_height, input_width,
+                                            input_channels])
+online_q_values, online_vars = q_network(X_state, name="q_networks/online")
+target_q_values, target_vars = q_network(X_state, name="q_networks/target")
+
+copy_ops = [target_var.assign(online_vars[var_name])
+            for var_name, target_var in target_vars.items()]
+copy_online_to_target = tf.group(*copy_ops)
+
+learning_rate = 0.001
+momentum = 0.95
+
+with tf.variable_scope("train"):
+    X_action = tf.placeholder(tf.int32, shape=[None])
+    y = tf.placeholder(tf.float32, shape=[None, 1])
+    q_value = tf.reduce_sum(online_q_values * tf.one_hot(X_action, n_outputs),
+                            axis=1, keepdims=True)
+    error = tf.abs(y - q_value)
+    clipped_error = tf.clip_by_value(error, 0.0, 1.0)
+    linear_error = 2 * (error - clipped_error)
+    loss = tf.reduce_mean(tf.square(clipped_error) + linear_error)
+
+    global_step = tf.Variable(0, trainable=False, name='global_step')
+    optimizer = tf.train.MomentumOptimizer(learning_rate, momentum, use_nesterov=True)
+    training_op = optimizer.minimize(loss, global_step=global_step)
+
+init = tf.global_variables_initializer()
+        
+saver = tf.train.Saver()
+
+
+def epsilon_greedy(q_values, step):
+    epsilon = max(eps_min, eps_max - (eps_max-eps_min) * step/eps_decay_steps)
+    if np.random.rand() < epsilon:
+        return np.random.randint(n_outputs) # random action
+    else:
+        return np.argmax(q_values) # optimal action
+        
+
+#checkpoint_path = "./checkpoints/my_dqn.ckpt"
+#checkpoint_path = "./checkpoints/my_dqn.ckpt2"
+#checkpoint_path = "./checkpoints/high_dqn.ckpt"
+checkpoint_path = "./checkpoints/high_dqn1.ckpt"
+done = True # env needs to be reset
+
+# Test the agent 
+frames = []
+n_max_steps = 10000
+
+with tf.Session() as sess:
+    saver.restore(sess, checkpoint_path)
+
+    obs = env.reset()
+    for step in range(n_max_steps):
+        state = preprocess_observation(obs)
+
+        # Online DQN evaluates what to do
+        q_values = online_q_values.eval(feed_dict={X_state: [state]})
+        action = np.argmax(q_values)
+
+        # Online DQN plays
+        obs, reward, done, info = env.step(action)
+
+        img = env.render(mode="rgb_array")
+        frames.append(img)
+
+        if done:
+            break
+
+# Print times
+timelog.append(datetime.datetime.now())
+for i in timelog:
+    print(i.strftime("%Y-%m-%d %H:%M"))
+
+
+while True:                  
+    video = plot_animation(frames)
+    plt.show()
+    
